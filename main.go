3389008e64926eade2ee03f5f455b098024fe03d
Ayo Adesokan <adesokanayo@gmail.com>
2021-12-27 09:20:16 -0700

    remove cors

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	srv := handler.NewDefaultServer(gqlgen.NewExecutableSchema(gqlgen.Config{Resolvers: &gqlgen.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", CorsMiddleware(srv))
+	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
